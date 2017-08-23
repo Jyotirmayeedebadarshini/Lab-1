a11a9dce6a79b2bf4206d2412b176dac450e8856
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-08-23 22:33:14 +0530

    Create problem 1.cpp

---
+++ problem 1.cpp
@@ -0,0 +1,18 @@
+{
+	int a;
+	float b;
+	char c;
+	bool d=true;
+	cout << "Enter integer-";
+	cin >> a;
+	cout << "Integer=" << a << endl;
+	cout << "Enter float-";
+	
+	cin >> b;
+	cout << "Float=" << b << endl;
+	cout << "Enter character-";
+	cin >> c;
+	cout << "Character=" << c << endl;
+	cout << d;
+	return 0;
+}
